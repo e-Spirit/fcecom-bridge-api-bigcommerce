a1f8c0a8fb2cb268f641c9b5d1757a1f0e47858c
Team E-Commerce <team-e-commerce@e-spirit.com>
2022-05-11 14:58:54 +0200

    "Updating to v1.1.0"

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
-const oas3Tools = require('oas3-tools');
-const https = require('https');
-const fs = require('fs');
+const BridgeCommons = require('fcecom-bridge-commons');
 const path = require('path');
 // Validate configuration
 const propertiesToCheck = ['GRAPHQL_URL', 'API_BASE_URL', 'ACCESS_TOKEN', 'CLIENT_ID'];
@@ -13,48 +11,20 @@ for (const propertyName of propertiesToCheck) {
   }
 }
 
-// Setup basic auth
-const { BRIDGE_AUTH_USERNAME, BRIDGE_AUTH_PASSWORD, auth = BRIDGE_AUTH_PASSWORD && `${BRIDGE_AUTH_USERNAME}:${BRIDGE_AUTH_PASSWORD}` } = process.env;
-function validate(request) {
-  if (!auth) {
-    return true;
-  }
-  return request.headers.authorization === `Basic ${Buffer.from(auth).toString('base64')}`;
-}
+const { BRIDGE_AUTH_USERNAME, BRIDGE_AUTH_PASSWORD, CONN_MODE, SSL_KEY, SSL_CERT } = process.env;
 
-// swaggerRouter configuration
-const options = {
-  routing: {
-    controllers: path.join(__dirname, './src/controllers'),
+const port = process.env.NODE_PORT || process.env.PORT || 3000;
+
+BridgeCommons({
+  username: BRIDGE_AUTH_USERNAME,
+  password: BRIDGE_AUTH_PASSWORD,
+  servicesDir: path.join(process.cwd(), './src/service'),
+  port: port,
+  features: {
+    contentPages: true,
+    categoryTree: true
   },
-  openApiValidator: {
-    validateSecurity: {
-      handlers: {
-        basicAuth: validate,
-      },
-    },
-  },
-};
-
-const expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'src/api/openapi.yaml'), options);
-const app = expressAppConfig.getApp();
-
-// Setup basic logging
-app.use(({ method, url }, res, next) => (console.log(`${method} ${url}`), next()));
-
-// Start listening
-const PORT = process.env.NODE_PORT || process.env.PORT || 3000;
-
-if (process.env.CONN_MODE == 'HTTPS') {
-  https
-    .createServer(
-      {
-        key: fs.readFileSync(path.resolve('./ssl/ssl.key')),
-        cert: fs.readFileSync(path.resolve('./ssl/ssl.pem')),
-      },
-      app
-    )
-    .listen(PORT, () => console.log(`listening at https://localhost.e-spirit.live:${PORT}`));
-} else {
-  app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
-}
+  useSsl: CONN_MODE === 'HTTPS',
+  sslCert: SSL_CERT,
+  sslKey: SSL_KEY
+});
